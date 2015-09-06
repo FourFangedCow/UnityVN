93721e5c63eaaf24992c820fc22094867dcc8fff
Craig Jong <Craig.Jong@digipen.edu>
2015-09-05 22:11:31 -0700

    Scene Manager
    Added the scene manager to oversee characters, animation, and text nodes. Replaced old text node with a generic base. Moved the testing story to a text file.

--- Assets/Text/SceneManager.cs
+++ Assets/Text/SceneManager.cs
@@ -10,13 +10,100 @@
  * 		- ?
  */
 //------------------------------------------------------------------------------
-using System;
-namespace AssemblyCSharp
-{
-	public class SceneManager
-	{
-		public SceneManager ()
-		{
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+/* Scene nodes are the base for all scene events. This includes character
+ * animation, text, special effects, etc.
+ */
+public abstract class SceneNode {
+	public SceneNode () {
+	}
+
+	// Activate function. Returns the delay if timed. Returns -1 if infinite.
+	public virtual float Activate(SceneManager SM) {
+		return -1;
+	}
+
+}
+
+public class TextNode : SceneNode {
+
+	public string Name = "DEFAULT NAME";
+	public string Text = "DEFAULT TEXT";
+
+	public TextNode(string name, string text) {
+		Name = name;
+		Text = text;
+	}
+	public override float Activate(SceneManager SM) {
+		SM.TM.Activate (this);
+		return -1.0f;
+	}
+}
+
+
+/* The scene manager maintains the scene node queue and makes sure events
+ * are fired correctly and in a timely manner. Should take a text file and
+ * generate all the characters accordingly.
+ */
+public class SceneManager : MonoBehaviour {
+
+	public TextManager TM;
+	Queue<SceneNode> SceneNodeQueue = new Queue<SceneNode>();	// The scene node queue
+
+	float SceneTimer = 0.0f;
+	float SceneDelay = -1.0f;	// The time delay until the next node.
+
+	public SceneManager () {
+
+	}
+
+	void Start () {
+		TM = new TextManager(this);
+		LoadScene ("Assets//Text//Story//TEST.txt");
+		NextSceneNode();
+	}
+
+	// Activated the next scene node in the queue
+	public void NextSceneNode () {
+		SceneTimer = 0.0f;
+		SceneDelay = SceneNodeQueue.Dequeue().Activate(this);
+	}	
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) { // INPUT SHOULD BE CHANGED
+			TM.InputPressed();
+		}
+
+		TM.Update();
+	
+		// Updating the scene node queue
+		//SceneTimer += Time.deltaTime;
+		//if (SceneDelay != -1.0f && SceneTimer > SceneDelay) {
+		//	NextSceneNode ();
+		//}
+	}
+
+
+	// --- ! READING IN TEXT ! --- //
+	void LoadScene (string fileName) {
+		string line;
+		StreamReader sr = new StreamReader(fileName);
+		using (sr) {
+			do {
+				line = sr.ReadLine ();
+				if(line != null) {
+					string[] args = line.Split ('|');
+					SceneNodeQueue.Enqueue(new TextNode(args[0], args[1]));
+				}
+			}
+			while (line != null);
+			sr.Close();
 		}
 	}
 }
--- Assets/Text/TextManager.cs
+++ Assets/Text/TextManager.cs
@@ -2,57 +2,70 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class TextManager : MonoBehaviour {
+public class TextManager {
 	// VARIABLES
-	Text Chat_;
-	Text Name_;
-	TextNode CurNode_;
+	bool Active;
+	Text Chat;
+	Text Name;
+	TextNode CurNode;
+	SceneManager SM;
 
-	// Writing shit
-	int WritingIndex_ = 0;
-	float WritingTimer_ = 0f;
-	float WritingTime_ = 0.05f;
-	bool WritingText_ = false;
+	// WRITING
+	int WritingIndex = 0;
+	float WritingTimer = 0f;
+	float WritingTime = 0.05f;
+	bool WritingText = false;
 
-	Scene Scene_ = new Scene1();
+	
+
+	// CONSTRUCTOR
+	public TextManager(SceneManager sm) {
+		Initialize(sm);
+	}
 
 	// Use this for initialization
-	void Start () {
-		Chat_ = GameObject.Find("Text_Chat").GetComponent<Text>();
-		Name_ = GameObject.Find("Text_Name").GetComponent<Text>();
-		Scene_.LoadText ();
+	void Initialize (SceneManager sm) {
+		SM = sm;
+		Active = false;
+		Name = GameObject.Find("Text_Name").GetComponent<Text>();
+		Chat = GameObject.Find("Text_Chat").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyUp (KeyCode.Space)) {
-			NextText ();
-		}
-		if (WritingText_) {
-			WritingTimer_ += Time.deltaTime;
-			if (WritingTimer_ > WritingTime_) {
-				WritingTimer_ -= WritingTime_;
-				Chat_.text = string.Concat(Chat_.text, NextChar());
+	public void Update () {
+		if (WritingText && Active) {
+			WritingTimer += Time.deltaTime;
+			if (WritingTimer > WritingTime) {
+				WritingTimer -= WritingTime;
+				Chat.text = string.Concat(Chat.text, NextChar());
 			}
 		}
 	}
 
-	void NextText () {
-		if (WritingText_ == true) {
-			WritingText_ = false;
-			Chat_.text = CurNode_.Text_;
-		} else {
-			CurNode_ = Scene_.GetNextText ();
-			Chat_.text = "";
-			Name_.text = CurNode_.Name_;
-			WritingText_ = true;
-			WritingIndex_ = 0;
+	// Input Function
+	public void InputPressed() {
+		if (WritingText && Active) {
+			WritingText = false;
+			Chat.text = CurNode.Text;
+		} else if (Active) {
+			Active = false;
+			SM.NextSceneNode ();
 		}
 	}
 
+	// Given new text node
+	public void Activate(TextNode tn) {
+		CurNode = tn;
+		Active = true;
+		WritingText = true;
+		Chat.text = "";
+		Name.text = CurNode.Name;
+		WritingIndex = 0;
+	}
+
 	string NextChar () {
-		if (WritingIndex_ >= CurNode_.Text_.Length - 1)
-			WritingText_ = false;
-		return CurNode_.Text_ [WritingIndex_++].ToString();
+		if (WritingIndex >= CurNode.Text.Length - 1)
+			WritingText = false;
+		return CurNode.Text [WritingIndex++].ToString();
 	}
 }
